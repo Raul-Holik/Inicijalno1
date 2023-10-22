58ea0d778ee91d54da5b7c7e4466c3126d9ae4cb
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-10-23 00:45:54 +0200

    Add files via upload

---
+++ Inicijalno1/Inicijalno1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno1
+{
+    internal class Program
+    {
+        class Biljka
+        {
+        }
+
+        class Stablo : Biljka
+        {
+            bool opadajuListovi;
+
+            public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+
+            public Stablo(bool opadajuListovi1)
+            {
+                opadajuListovi = opadajuListovi1;
+            }
+
+        }
+
+        class Cvijet : Biljka
+        {
+        }
+
+        class Crnogoricno : Stablo
+        {
+            bool _opadajuListovi;
+
+            public Crnogoricno(bool opadajuListovi1) : base(opadajuListovi1)
+            {
+                _opadajuListovi = opadajuListovi1;
+
+            }
+            public override string ToString()
+            {
+                return "Opadaju listovi = " + _opadajuListovi;
+            }
+        }
+
+        class Bijelogoricno : Stablo
+        {
+            bool _opadajuListovi;
+
+            public Bijelogoricno(bool opadajuListovi1) : base(opadajuListovi1)
+            {
+                _opadajuListovi = opadajuListovi1;
+            }
+            public override string ToString()
+            {
+                return "Opadaju listovi = " + _opadajuListovi;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Crnogoricno Bor = new Crnogoricno(false);
+            Bijelogoricno Hrast = new Bijelogoricno(true);
+
+            Console.WriteLine("Bor: " + Bor.ToString());
+            Console.WriteLine("Hrast: " + Hrast.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
